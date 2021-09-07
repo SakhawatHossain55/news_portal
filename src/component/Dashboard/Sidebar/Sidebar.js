71721740b1ab8d2ac0f2e672867f5becf8014114
SakhawatHossain55 <sakhawatHossain7969@gmail.com>
2021-09-07 21:22:29 +0600

    test

--- src/component/Dashboard/Sidebar/Sidebar.js
+++ src/component/Dashboard/Sidebar/Sidebar.js
@@ -1,6 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import logo from "../../../images/logo.png";
 import "./Sidebar.css";
 
 import { UserContext } from "../../../App";
